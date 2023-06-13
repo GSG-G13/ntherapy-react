c89d1b7c49de932f3cb8fcebd7fd53e76c5b95cf
raghdarizq <raghdarizq2@gmail.com>
2023-06-13 14:50:30 +0300

    build: create a table dashboard for Admin

---
+++ src/components/dashboard/BodyTable.tsx
@@ -0,0 +1,48 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Typography from '@mui/material/Typography';
+import {  Grid } from '@mui/material';
+import TableRow from '@mui/material/TableRow';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+
+const BodyTable = () => {
+  return (
+    <TableBody>
+    <TableRow
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell component="th" scope="row" >
+        <Grid container alignItems="center" >
+          <Grid item>
+            <Avatar alt="Remy Sharp" src="https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?w=2000" />
+          </Grid>
+          <Grid item style={{ marginLeft: "10px" }}>
+            <div>
+              <Typography sx={{fontWeight:600}} color='primary.dark'>Raghda N. Abu Rizq</Typography>
+              <Typography color='primary.main'>123365789</Typography>
+              <Typography color='primary.main'>lorem lorem</Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </TableCell>
+      <TableCell align="center">
+        <Button variant="contained">
+          Download
+          <FileDownloadIcon />
+        </Button>
+      </TableCell>
+      <TableCell align="center">
+        <Typography color='primary.main'>Pending</Typography>
+      </TableCell>
+      <TableCell align="center" style={{ gap: "3px" }}>
+      <Button variant="contained" color="error" style={{marginRight:"5px"}}>Decline</Button>
+      <Button variant="contained" color="primary">Approve</Button>
+      </TableCell>
+    </TableRow>
+  </TableBody>
+  )
+}
+
+export default BodyTable
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,24 @@
-const Admin = () => (
-  <div>Admin</div>
-);
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import { Container } from '@mui/material';
+import TableHeader from '../../components/dashboard/TableHeader';
+import BodyTable from "../../components/dashboard/BodyTable";
 
-export default Admin;
+const Admin = () => {
+  return (
+    <>
+      <div>Admin</div>
+      <Container>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHeader />
+            <BodyTable />
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
+  )
+}
+
+export default Admin
