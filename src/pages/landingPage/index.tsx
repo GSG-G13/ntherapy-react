d0b48ae5dc6cbabc77ec744f30d9c495b31a8c8e
MohannadSabaa <Moh4nn4d@gmail.com>
2023-06-13 22:43:16 +0300

    feat: Landing page refactoring code completed unfinished components

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,81 +1,121 @@
-import { Container, Grid } from '@mui/material'
-import { hero, its, discover, join, appointments } from './styles'
 import { Link } from 'react-router-dom'
+import { Container, Grid, Typography} from '@mui/material'
+import { 
+  hero, 
+  therapyP,
+  its, 
+  discover, 
+  join, appointments , 
+  therapyImg, 
+  logo, 
+  cardHeader, 
+  signJoinHeader, 
+  signJoinP, 
+  logoH,
+  logoF
+} from './styles'
 
 const LandingPage = ()=> {
   
-
   return (
     <>
-      <Container sx={hero}> 
-      <h1 className='way'>WAY </h1>
-      <h1 className='life'>TO LIFE...</h1>
-      <Container sx={{position:"absolute",left:0, minWidth:"100%",display:'flex', gap: "30px", bottom:"-40px",  justifyContent:"center"}}>
-        <div className='appointment'>Appiontemnt</div>
-        <div className='appointment'>Doctors</div>
-        <div className='appointment'>Services</div>
+      <Container  sx={hero}> 
+  <Container sx={logo}>
+  <Typography sx={logoH} variant="h2" className='way'>YOUR WAY TO </Typography>
+      <Typography sx={logoF} variant="h2" className='life'>LIFE</Typography>
+  </Container>
       </Container>
-      </Container>
-      <Grid container sx={appointments}>
-     <Grid xs={5} item>
-      <img className='therapy-image' src="https://images.unsplash.com/photo-1592947945242-69312358628b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"></img>
+      <Grid xs={10}container sx={appointments}>
+     <Grid xs={3} item>
+    <div style={therapyImg}>
+    </div>
       </Grid>
-  <Grid  item xs={5} >
+  <Grid  item xs={4} >
   <div className='therapy-card'>
-        <h3 className='title'>Why Therapy is important ?</h3>
-        <p>Therapy is an essential part of any successful recovery process, helping to reduce stress, improve mental health, and increase overall wellbeing. Learn why therapy is so important and how it can help you reach your goals.</p>
+        <Typography sx={cardHeader} variant="h6">Why Therapy is important ?</Typography>
+        <Typography>Therapy is an essential part of any successful recovery process, helping to reduce stress, improve mental health, and increase overall wellbeing. Learn why therapy is so important and how it can help you reach your goals.</Typography>
          </div>
         <div className='therapy-card'>
-          <h3 className='title'>Is therapy right for you?</h3>
-          <p>Find out the pros and cons of this type of therapy to help you decide if it's the right fit for you</p>
+          <Typography sx={cardHeader} variant="h6">Is therapy right for you?</Typography>
+          <Typography>Find out the pros and cons of this type of therapy to help you decide if it's the right fit for you</Typography>
           </div>
         <div className='therapy-card'>
-          <h3  className='title'>Is therapy right for you?</h3>
-          <p>Find out the pros and cons of this type of therapy to help you decide if it's the right fit for you</p>
+          <Typography sx={cardHeader} variant="h6">What Are the Benefits of Therapy?</Typography>
+          <Typography>Therapy can be a powerful tool for improving mental health and wellbeing. Discover the many benefits of therapy and how it can help you lead a healthier, happier life.</Typography>
+          </div>
+    </Grid>
+  <Grid  item xs={4} >
+  <div className='therapy-card'>
+        <Typography sx={cardHeader} variant="h6">Why Therapy is important ?</Typography>
+        <Typography>Therapy is an essential part of any successful recovery process, helping to reduce stress, improve mental health, and increase overall wellbeing. Learn why therapy is so important and how it can help you reach your goals.</Typography>
+         </div>
+        <div className='therapy-card'>
+          <Typography sx={cardHeader} variant="h6"> Therapy impacts on Mental Health</Typography>
+          <Typography>Therapy can have a powerful impact on mental health, providing individuals with the tools and support they need to manage their mental health and lead a healthier, more fulfilling life.</Typography>
+          </div>
+        <div className='therapy-card'>
+          <Typography sx={cardHeader} variant="h6">When Should Therapy Be Used?</Typography>
+          <Typography>Therapy can be a powerful tool for improving mental health, but it's important to understand when it should be used. </Typography>
           </div>
     </Grid>
       </Grid>
       <Container sx={its}>
     <div>
     
-    <img className='its-img' src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/professional.svg?v=0177f73d2461"></img>
-    <h2 className='its'>it's Professional</h2>
-      <p >All therapists are licensed, accredited professionals. BetterHelp allows you to connect with them in a safe and convenient online environment.</p>
+    <img 
+    className='its-img' 
+    src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/professional.svg?v=0177f73d2461">
+    </img>
+    <Typography sx={{fontWeight:"600"}}>it's Professional</Typography>
+      <Typography sx={therapyP}>All therapists are licensed, accredited professionals. BetterHelp allows you to connect with them in a safe and convenient online environment.</Typography>
     </div>
     <div>
-    <img className='its-img' src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/affordable.svg?v=0177f73d2461"></img>
+    <img 
+    className='its-img' 
+    src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/affordable.svg?v=0177f73d2461">
+      </img>
 
-    <h2 className='its'>it's Affordable.</h2>
-      <p >All therapists are licensed, accredited professionals. BetterHelp allows you to connect with them in a safe and convenient online environment.</p>
+    <Typography sx={{fontWeight:"600"}}>it's Affordable.</Typography>
+      <Typography sx={therapyP}>Pay a low flat fee for unlimited therapy with your therapist. Therapy doesn't have to be expensive</Typography>
     </div>
     <div>
 
-    <img className='its-img' src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/convenient.svg?v=0177f73d2461"></img>
-    <h2 className='its'>it's Convenient</h2>
-      <p>All therapists are licensed, accredited professionals. BetterHelp allows you to connect with them in a safe and convenient online environment.</p>
+    <img 
+    className='its-img' 
+    src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/convenient.svg?v=0177f73d2461">
+    </img>
+    <Typography sx={{fontWeight:"600"}}>it's Convenient</Typography>
+      <Typography  sx={therapyP}>ADo it at your own time and at your own pace. Communicate with your therapist as often as you want and whenever you feel it's needed.</Typography>
     </div>
     <div>
     
-    <img className='its-img' src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/effective.svg?v=0177f73d2461"></img>
-    <h2 className='its'>it's Effective.</h2>
-      <p >All therapists are licensed, accredited professionals. BetterHelp allows you to connect with them in a safe and convenient online environment.</p>
+    <img 
+    className='its-img' 
+    src="https://dy7glz37jgl0b.cloudfront.net/betterhelp_two/illustrations/effective.svg?v=0177f73d2461">
+    </img>
+    <Typography sx={{fontWeight:"600"}}>it's Effective.</Typography>
+      <Typography sx={therapyP}>Thousands of people have benefitted from therapy (just check out their reviews!) With BetterHelp, you can switch therapists at any point if you don't feel you are getting enough benefit.</Typography>
     </div>
-      
       </Container>
-
-<Container sx={discover}>
-  <img className='join-us' src='https://images.unsplash.com/photo-1520857014576-2c4f4c972b57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80) no-repeat center center/cover'></img>
+      <Container sx={discover}>
+  <img 
+  className='sign-img' 
+  src='https://images.unsplash.com/photo-1507537362848-9c7e70b7b5c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80) no-repeat center center/cover'>
+  </img>
     <div>
-    <h1 className='discover'>Discover your self</h1>
-      <p className='discover-p'>Join us to get provided with the tools and support you need to manage your mental health and lead a healthier, more fulfilling life.</p>
-      <Link className='join' to="/">Sign UP</Link>
+    <Typography  sx={signJoinHeader}>Discover your self</Typography>
+      <Typography sx={signJoinP}>Join us to get provided with the tools and support you need to manage your mental health and lead a healthier, more fulfilling life.</Typography>
+      <Link className='join' to="/singup">Sign UP</Link>
     </div>
       </Container >
       <Container sx={join}>
-        <img className='join-us' src="https://plus.unsplash.com/premium_photo-1664378802612-64eb63ec982e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"></img>
+        <img 
+        className='join-img' 
+        src="https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80">
+        </img>
       <div>
-      <h1 className='discover'>Be one of the team</h1>
-      <p className='discover-p'>Join us to get provided with the tools and support you need to manage your mental health and lead a healthier, more fulfilling life.</p>
+      <Typography sx={signJoinHeader}>Be one of the team</Typography>
+      <Typography sx={signJoinP}>Join us to get provided with the tools and support you need to manage your mental health and lead a healthier, more fulfilling life.</Typography>
       <Link className='join' to="/">Join us</Link>
       </div>
       </Container>
--- src/pages/landingPage/styles.ts
+++ src/pages/landingPage/styles.ts
@@ -1,26 +1,93 @@
 
-/* import vector from '../../../public/vector2.jpg' */
-
 interface Style {
     [key: string] : string | number
 }
 
 const hero = <Style> {
     position:"relative",
-    border: "solid black 1px rgb",
-   height: "600px",
+    height: "600px",
     minWidth: "100%",
-    background: "url('https://plus.unsplash.com/premium_photo-1664378617259-6bd0f244b1fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=800') no-repeat center center /cover",
+    background: "url('https://plus.unsplash.com/premium_photo-1664378617259-6bd0f244b1fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=800') no-repeat center 0px /cover",
+    borderBottom: "10px solid rgb(255, 208, 77)",
 }
-const therapy = <Style> { display:"grid", justifyContent:'center' ,alignItems:'center',padding:"60px 10px",minWidth:"100%",gap:"10px", minHeight:"700px"}
-const background = {
-    background:'url("") no-repeat center center`',  
-    opacity: ".5",
+const therapy = <Style> { 
+    display:"grid",
+    justifyContent:'center',
+    alignItems:'center',
+    padding:"60px 10px",
+    maxWidth:"100%",
+    gap:"10px", 
+    minHeight:"700px"
 }
-const its = <Style> {display:"flex", gap:"20px",alignItems:"center",margin:"40px auto", height: "400px"}
+const appointments = <Style> {
+    gap: "10px",
+    width:"70%",
+    alignItems:"center",
+    margin: '100px auto ',
+    // boxShadow: "1px 0 10px 0 rgba(0,0, 0, .1)"
 
-const discover = <Style> {display:"flex", flexDirection: "row-reverse",justifyContent:"center",alignItems:"center",margin:"30px auto", height: "400px", minWidth:"80%"}
+}
+const logo = <Style> {textAlign:"center",width:"500px",position:"absolute",top:"270px", right: "50px"}
+const logoH = <Style> {fontWeight:"bolder",fontSize:"80px",width:"450px", background:"rgb(250, 183, 38)"}
+const logoF = <Style> {fontWeight:"bold",fontSize:"80px",width:"450px",border:"1px solid black",
+color:"white",
+background:"black"}
+const its = <Style> {
+    display:"flex",
+    gap:"20px",
+    alignItems:"center",
+    margin:"40px auto", 
+    height: "400px",
+    width:"90%",
+    borderBottom: "1px solid rgb(255, 208, 77)"
+}
+const therapyP = <Style> {fontSize:"16px",  lineHeight:"20px",textAlign:"left", marginTop:"7x"};
 
-const join = <Style> {display:"flex", alignItems:"center",margin:"30px auto", height: "400px", minWidth:"80%"}
-const appointments = <Style> {alignItems:'center',padding:"20px", width:"70%",  border:"1px solid rgba(0, 0, 0, .1)", margin: '100px auto 30px auto', borderRadius:'10px'}
-export {hero, therapy, background, its, discover, join, appointments}
+const discover = <Style> {
+    position: "relative",
+    display:"flex", 
+    flexDirection: "row-reverse",justifyContent:"center",
+    alignItems:"center",
+    margin:"100px auto",
+    height: "400px", 
+    maxWidth:"100%",
+    border: "1px solid rgba(255, 208, 77, .3)"
+}
+
+const join = <Style> {
+    margin:"200px auto",
+    display:"flex",
+    alignItems:"center",
+    height: "400px",
+    maxnWidth:"80%",
+    border: "1px solid #003ab828"
+    
+}
+const signJoinHeader = <Style> {
+    color:"#003ab8",
+fontSize:"40px",
+textTransform: "uppercase",
+paddingLeft: "10px",
+borderLeft: "10px solid rgb(255, 208, 77)"}
+const signJoinP = <Style> {fontSize:"23px", textTransform:"uppercase", lineHeight:"30px", marginTop:"10px"}
+const cardHeader = {color:"#003ab8",textTransform:"uppercase"}
+const therapyImg = <Style>{
+    height: "475px",
+    width:"330px",
+    background: "url(https://images.unsplash.com/photo-1592947945242-69312358628b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80) no-repeat center center /cover",
+    borderBottom: "10px solid rgb(255, 208, 77)"
+}
+
+export {hero, 
+    therapy, 
+    therapyP,
+    therapyImg, 
+    its, 
+    discover, 
+    join, 
+    appointments, 
+    logo, cardHeader, 
+    signJoinHeader, 
+    signJoinP,
+    logoH, 
+     logoF}
