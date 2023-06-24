ae308a55f1eabbb3d198a2bb69703550dc976381
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-06-24 23:16:34 +0300

    fix: rename function getTimeRange

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,7 +4,6 @@ import Payment from './bookappointment/payment';
 import BookAppointment from './bookappointment/selectTime';
 import CheckoutForm from './checkoutform';
 
-// eslint-disable-next-line import/prefer-default-export
 export {
   AppointmentsModal, SessionReservationModal, Payment, BookAppointment, CheckoutForm,
 };
--- src/utils/TimeRange.ts
+++ src/utils/TimeRange.ts
@@ -1,9 +1,9 @@
 import moment from 'moment';
 
-const TimeRanges = (datetime:string) => {
+const getTimeRanges = (datetime:string) => {
   const from = moment(datetime).format('HH-mm');
   const to = moment(datetime).add(1, 'hours').format('HH-mm');
   return { from, to };
 };
 
-export default TimeRanges;
+export default getTimeRanges;
