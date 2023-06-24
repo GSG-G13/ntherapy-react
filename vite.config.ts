a59a06aa83d95aca097d0d33862e19fc343b316d
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-06-24 14:19:16 +0300

    fix: fix changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
+  const env = loadEnv(mode, process.cwd(), "");
+ 
   return {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': {
+        "/api": {
           target: env.VITE_BASEURL,
           changeOrigin: true,
-          secure: false,
-          ws: true,
-          configure(proxy, options) {
-            proxy.on('error', (err) => {
-              console.log('err', err);
-            });
-          },
-
         },
       },
     },
