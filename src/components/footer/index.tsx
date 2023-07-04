09497c58b8712666948ed9ff556191530243013a
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-07-04 22:09:09 +0300

    build: add css style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,57 +1,108 @@
-import { Box, Typography, IconButton } from '@mui/material';
-import {
-  Facebook, Twitter, Instagram,
-} from '@mui/icons-material';
+import { Box, Link, Typography } from '@mui/material';
+import { Facebook, Twitter, Instagram } from '@mui/icons-material';
 import logo from '../../assets/img/logo.png';
 
 const Footer = () => (
-  <Box
-    sx={{
-      backgroundColor: '#516EFF',
-      padding: '16px',
-      marginTop: '32px',
-      textAlign: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
+  <footer>
+    <Box sx={{
+      backgroundColor: '#f7f4f4', padding: '60px 0', boxShadow: '10px 10px 8px 7px #888888', mt: 3,
     }}
-  >
-    <img src={logo} alt="logo" style={{ width: '200px' }} />
-    <Typography variant="body2" color="#fff">
-      &copy; 2023 Ntherapy. All rights reserved.
-    </Typography>
-
-    <Box sx={{ marginTop: '16px' }}>
-      <IconButton
-        sx={{ color: '#fff' }}
-        aria-label="Facebook"
-        href="https://www.facebook.com/yourpage"
-        target="_blank"
-        rel="noopener noreferrer"
+    >
+      <Box sx={{
+        maxWidth: 'container', display: 'flex', flexDirection: 'row', alignItems: 'center',
+      }}
       >
-        <Facebook />
-      </IconButton>
-      <IconButton
-        sx={{ color: '#fff' }}
-        aria-label="Twitter"
-        href="https://www.twitter.com/yourpage"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Twitter />
-      </IconButton>
-      <IconButton
-        sx={{ color: '#fff' }}
-        aria-label="Instagram"
-        href="https://www.instagram.com/yourpage"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Instagram />
-      </IconButton>
+        <Box sx={{ flexBasis: '50%' }}>
+          <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
+            <img src={logo} alt="logo" style={{ maxWidth: '30%' }} />
+          </Box>
+          <Typography
+            variant="body1"
+            sx={{
+              color: '#516EFF',
+              fontWeight: 'bold',
+              fontSize: '18.52px',
+              lineHeight: '28px',
+              width: '500px',
+              textAlign: 'center',
+              mx: 'auto',
+            }}
+          >
+            We&apos;re committed to delivering life-changing anxiety
+            and depression care to everyone who needs it.
+          </Typography>
+        </Box>
+        <Box sx={{ flexBasis: '50%' }}>
+          <Box sx={{ marginBottom: '20px' }}>
+            <Typography
+              variant="h3"
+              sx={{
+                color: '#516EFF',
+                fontWeight: 600,
+                fontSize: '22.52px',
+                lineHeight: '28px',
+                textAlign: 'center',
+              }}
+            >
+              CONTACTS
+            </Typography>
+          </Box>
+          <Typography
+            variant="body1"
+            sx={{
+              color: '#516EFF',
+              fontWeight: 500,
+              fontSize: '18.52px',
+              lineHeight: '28px',
+              textAlign: 'center',
+            }}
+          >
+            <Link href="/" sx={{ color: '#516EFF' }}>Ntherapist@gmail.com</Link>
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              color: '#516EFF',
+              fontWeight: 500,
+              fontSize: '18.52px',
+              lineHeight: '28px',
+              textAlign: 'center',
+              marginTop: '16px',
+            }}
+          >
+            <Link href="/" sx={{ marginRight: '8px' }}>
+              <Facebook fontSize="small" sx={{ color: '#516EFF' }} />
+            </Link>
+            <Link href="/" sx={{ marginRight: '8px' }}>
+              <Twitter fontSize="small" sx={{ color: '#516EFF' }} />
+            </Link>
+            <Link href="/">
+              <Instagram fontSize="small" sx={{ color: '#516EFF' }} />
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
     </Box>
-  </Box>
+    <Box sx={{ backgroundColor: '#5885ff', padding: '20px 0' }}>
+      <Box sx={{
+        maxWidth: 'container', display: 'flex', flexDirection: 'row', justifyContent: 'center',
+      }}
+      >
+        <Typography
+          variant="body1"
+          sx={{
+            color: '#fff',
+            fontWeight: 500,
+            fontSize: '15.52px',
+            lineHeight: '28px',
+            textAlign: 'center',
+          }}
+        >
+          &copy; 2023. All rights reserved.
+        </Typography>
+      </Box>
+    </Box>
+  </footer>
 );
 
 export default Footer;
