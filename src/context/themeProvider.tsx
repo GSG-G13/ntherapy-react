c0afa7af15c7d294c9d9f597d5d41df35bcd1a03
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-07-23 11:23:11 +0300

    fix: fix selecting appointment

--- src/context/themeProvider.tsx
+++ src/context/themeProvider.tsx
@@ -1,11 +1,16 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useMemo, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { AppContextProps } from './types';
 import { theme, darkTheme } from '../theme/theme';
 import ThemeContext from './themeContext';
 
-const CheckThemeProvider : React.FC<AppContextProps> = ({ children }) => {
-  const [themeMode, setThemeMode] = React.useState<'light' | 'dark'>('light');
+const CheckThemeProvider: React.FC<AppContextProps> = ({ children }) => {
+  const initialThemeMode = localStorage.getItem('themeMode') || 'light';
+  const [themeMode, setThemeMode] = React.useState(initialThemeMode);
+
+  useEffect(() => {
+    localStorage.setItem('themeMode', themeMode);
+  }, [themeMode]);
 
   const handleThemeModeSwitch = useCallback(() => {
     setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
